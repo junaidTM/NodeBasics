94a7952b2a6954cd128af103febc61614edc5065
junaidTM <junaid.ansari@turtlemint.com>
2024-08-06 18:44:22 +0530

    Added : changes for require and export

---
+++ Main.js
@@ -0,0 +1,12 @@
+var operatorObj = require('./Operators');
+function greet(name){
+    console.log("Hello there, "+ name +"!");
+}
+greet("Junaid");
+
+console.log("Addition :" + operatorObj.add(20,36));
+console.log("Subtraction :" + operatorObj.subtract(46,23));
+
+
+
+
